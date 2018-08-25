7b797e635c60687c5704c5426cef902463cb1a92
Justin Doan <jmdoan1@gmail.com>
2018-08-24 23:19:37 -0400

    working timer logic with manual label updates

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Timer from '../components/Timer/Timer';
+
 class App extends Component {
 
   state = {
-    time: "0:00:00",
+    time: 0,
     showTime: true,
     withProsthesis: false,
     notes: "",
-    timerRunning: false
+    timerRunning: false,
+    startTime: 0,
+    stopTime: 0
   }
 
   toggleWithProsthesis = () => {
-    if (this.state.withProsthesis) {
-      this.setState({ withProsthesis: false });
-    } else {
-      this.setState({ withProsthesis: true });
-    }
+    this.setState({ withProsthesis: !this.state.withProsthesis });
   }
 
   toggleTimerRunning = () => {
     if (this.state.timerRunning) {
-      this.setState({ timerRunning: false });
+      this.setState({ timerRunning: false, stopTime: Date.now(), time: Date.now() - this.state.startTime });
     } else {
-      this.setState({ timerRunning: true });
+      this.setState({ timerRunning: true, startTime: Date.now(), stopTime: 0 });
+    }
+  }
+
+  updateTime = () => {
+    if (this.state.timerRunning) {
+      this.setState({ time: Date.now() - this.state.startTime })
     }
   }
 
   toggleTimerShowing = () => {
-    if (this.state.showTime) {
-      this.setState({ showTime: false });
-    } else {
-      this.setState({ showTime: true });
-    }
+    this.setState({ showTime: !this.state.showTime });
   }
 
   render() {
+    let buttonText = "Start Timer"
+  
+    if (this.state.timerRunning) {
+      buttonText = "Stop Timer"
+    }
+
     return (
       <div className="App">
         <h1>10 Meter Walk Test</h1>
-        <button onClick={this.toggleTimerRunning}>Start Timer</button>
-        <h2 onClick={this.toggleTimerShowing}>{this.state.time}</h2>
+        <button onClick={this.toggleTimerRunning}>{buttonText}</button>
+        <button onClick={this.updateTime}>Update Time</button>
+        <h2 onClick={this.toggleTimerShowing}>{this.state.time} milliseconds ---- {this.state.time / 1000} seconds</h2>
+        <Timer onClick={this.toggleTimerShowing}>{this.state.time}</Timer>
         <button onClick={this.toggleWithProsthesis}>Toggle Prosthesis</button>
         <div>
           <text>Notes:     </text>
